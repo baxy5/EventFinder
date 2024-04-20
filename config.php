fcf5c29270e8a98113ffda9615dd8057757adfbd
baxy5 <bakos.soda@gmail.com>
2024-04-20 22:31:54 +0200

    init

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php 
+ define('DB_PASS', 'F6AE_eo[L_-X*AUo');
+ define('DB_NAME', 'demo');
---
+++ regist.php
@@ -0,0 +1,59 @@
+<?php
+require_once ('config.php');
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $conn = new PDO(
+        'mysql:host=localhost;dbname=' . DB_NAME . ';charset=utf8',
+        DB_NAME,
+        DB_PASS
+    );
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    $hashed_password = sha1($_POST['password']);
+
+    $sql = "INSERT INTO auth (email, password) VALUES  ( '{$_POST['email']}', '{$hashed_password}' )";
+    $res = $conn->query($sql);
+
+    $conn = null;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>Sign up</title>
+</head>
+
+<body>
+    <?php include 'components/navbar.php'; ?>
+
+    <div class="container pt-5">
+        <h1>Sign up</h1>
+        <form action="" method="POST">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email cím</label>
+                <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Jelszó</label>
+                <input type="password" name="password" class="form-control" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Regisztráció</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
